d26bd42669ff7fedbe2e1446b00d0927cf5f44e7
Bansi12846 <lathiyabansi2006@gmail.com>
2026-05-08 14:20:33 +0530

    Prims algorithm

--- Pr7.java
+++ Pr7.java
@@ -1,51 +1,55 @@
-#include <stdio.h>
+import java.util.Scanner;
 
-int main() {
-    int n, cost[10][10], visited[10] = {0};
-    int i, j, min, a, b, u, v, ne = 1;
-    int mincost = 0;
+class PrimMST {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    printf("Enter number of vertices: ");
-    scanf("%d", &n);
+        int n, min, a = 0, b = 0, u = 0, v = 0;
+        int mincost = 0;
+        int[][] cost = new int[10][10];
+        int[] visited = new int[10];
 
-    printf("Enter cost matrix:\n");
+        System.out.print("Enter number of vertices: ");
+        n = sc.nextInt();
 
-    for(i = 1; i <= n; i++) {
-        for(j = 1; j <= n; j++) {
-            scanf("%d", &cost[i][j]);
+        System.out.println("Enter cost matrix:");
 
-            if(cost[i][j] == 0)
-                cost[i][j] = 999;
-        }
-    }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                cost[i][j] = sc.nextInt();
 
-    visited[1] = 1;
-
-    while(ne < n) {
-        min = 999;
-
-        for(i = 1; i <= n; i++) {
-            for(j = 1; j <= n; j++) {
-                if(cost[i][j] < min) {
-                    if(visited[i] != 0) {
-                        min = cost[i][j];
-                        a = u = i;
-                        b = v = j;
-                    }
-                }
+                if (cost[i][j] == 0)
+                    cost[i][j] = 999;
             }
         }
 
-        if(visited[u] == 0 || visited[v] == 0) {
-            printf("%d Edge (%d,%d) = %d\n", ne++, a, b, min);
-            mincost += min;
-            visited[b] = 1;
+        visited[1] = 1;
+
+        for (int ne = 1; ne < n; ) {
+            min = 999;
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (cost[i][j] < min) {
+                        if (visited[i] != 0) {
+                            min = cost[i][j];
+                            a = u = i;
+                            b = v = j;
+                        }
+                    }
+                }
+            }
+
+            if (visited[u] == 0 || visited[v] == 0) {
+                System.out.println(ne + " Edge (" + a + "," + b + ") = " + min);
+                mincost += min;
+                visited[b] = 1;
+                ne++;
+            }
+
+            cost[a][b] = cost[b][a] = 999;
         }
 
-        cost[a][b] = cost[b][a] = 999;
+        System.out.println("Minimum cost = " + mincost);
     }
-
-    printf("Minimum cost = %d", mincost);
-
-    return 0;
-        }
+}
